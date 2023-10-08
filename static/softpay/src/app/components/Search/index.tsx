18d58e35e875c0bd24714ce222900f83b3fac3ca
rodrigofilhoo <rodrigofilhogt@gmail.com>
2023-10-08 18:24:39 -0300

    Search Bar

---
+++ static/softpay/src/app/components/Search/index.tsx
@@ -0,0 +1,24 @@
+'use client'
+import React from "react"
+import Link from "next/link"
+
+function Search() {
+    return (
+        <Link href={""}>
+            <form>
+                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Buscar</label>
+                <div className="relative">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg className="w-4 h-4 text-gray-700 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                    </div>
+                    <input type="search" id="default-search" className="block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-purple-700 focus:border-blue-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-700 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Busque o que quiser...." required />
+                    <button type="submit" className="text-white absolute right-2.5 bottom-2 bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-1.5 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">Buscar</button>
+                </div>
+            </form>
+        </Link>
+    )
+}
+
+export default Search
--- static/softpay/src/app/page.tsx
+++ static/softpay/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar"
+import Search from './components/Search'
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Search />
     </>
-
   )
 }
